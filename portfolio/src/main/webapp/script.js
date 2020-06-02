3ce497268bb791fee595549d003e24093e3a71f3
Jimin Byun <jiminb159@gmail.com>
2020-06-02 03:34:13 +0000

    Add arrays for different topics, Add function to get index of random topic

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -20,13 +20,32 @@ function addRandomGreeting() {
         ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
     // Pick a random greeting.
-    const greeting = greetings[getRandomIndex(greetings)];
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
     // Add it to the page.
     const greetingContainer = document.getElementById('greeting-container');
     greetingContainer.innerText = greeting;
+    addRandomRandomButton();
 }
 
 function getRandomIndex(array){
     return Math.floor(Math.random()*array.length);
+}
+
+var index = -1;
+const books = 
+    ['Rangers', 'Exhalation', 'The Elegant Universe', 'Human Acts'];
+const tvshows = 
+    ['Brookyln Nine Nine', 'Designated Survivor', 'Person of Interest', 'The Good Place', 'Broadchurch'];
+const korean =
+    ['안녕하세요!', '좋은 하루 되세요!', '오늘 날씨는 어떤가요?', '졸려요', '고맙습니다'];
+const misc = 
+    ['Penguin', 'Blue', 'Baseball', 'Cheesecake'];
+const cumultative = 
+    [books, tvshows, korean];
+
+function getRandomTopic(){
+    const topics = ['book recommendation', 'tv show recommendation', 'korean phrase', 'thing I like'];
+    index = getRandomIndex(cumultative);
+    console.log(index);
 }
