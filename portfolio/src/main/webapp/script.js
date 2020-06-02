ae22431d35798b5a8a5fdd5b88996b7a04b22c76
Jimin Byun <jiminb159@gmail.com>
2020-06-02 03:14:21 +0000

    Seperate function for generating index for random index of the array

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -20,9 +20,13 @@ function addRandomGreeting() {
         ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
     // Pick a random greeting.
-    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    const greeting = greetings[getRandomIndex(greetings)];
 
     // Add it to the page.
     const greetingContainer = document.getElementById('greeting-container');
     greetingContainer.innerText = greeting;
+}
+
+function getRandomIndex(array){
+    return Math.floor(Math.random()*array.length);
 }
