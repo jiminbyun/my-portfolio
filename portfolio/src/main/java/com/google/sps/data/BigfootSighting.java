a1ddb3458898eab904fdd8caf75806bfded500dc
Jimin Byun <jiminb159@gmail.com>
2020-06-16 13:57:26 +0000

    Parse bigfoot sightings data to array of lat and lngs

---
+++ portfolio/src/main/java/com/google/sps/data/BigfootSighting.java
@@ -0,0 +1,12 @@
+package com.google.sps.data;
+
+public class BigfootSighting {
+  
+  private double lat;
+  private double lng;
+
+  public BigfootSighting(double lat, double lng) {
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
---
+++ portfolio/src/main/java/com/google/sps/servlets/BigfootDataServlet.java
@@ -0,0 +1,43 @@
+package com.google.sps.servlets;
+
+import com.google.sps.data.BigfootSighting;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Returns Bigfoot data as a JSON array*/
+@WebServlet("/bigfoot-data")
+public class BigfootDataServlet extends HttpServlet {
+  
+  private Collection<BigfootSighting> bigfootSightings;
+
+  @Override 
+  public void init() {
+    bigfootSightings = new ArrayList<>();
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/bf-data.csv"));
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
+
+      double lat = Double.parseDouble(cells[4]);
+      double lng = Double.parseDouble(cells[5]);
+
+      bigfootSightings.add(new UfoSighting(lat, lng));
+    }
+    scanner.close();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    String json = gson.toJson(bigfootSightings);
+    response.getWriter().println(json);
+  }
+}
