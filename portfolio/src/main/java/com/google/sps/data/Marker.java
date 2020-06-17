c69451ea9366b89ba0564c48f93c19c5aa8422d6
Jimin Byun <jiminb159@gmail.com>
2020-06-17 00:46:25 +0000

    Add marker class

---
+++ portfolio/src/main/java/com/google/sps/data/Marker.java
@@ -0,0 +1,32 @@
+package com.google.sps.data;
+
+/** Represents a marker on the map. */
+public class Marker {
+
+  private final double latitude;
+  private final double longitude;
+  private final String name;
+  private final String content;
+
+  public Marker(double latitude, double longitude, String name, String content) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.content = content;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
