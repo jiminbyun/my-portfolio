b6246c9fd8792c9e9897ff21c0bcf4dc87e01adc
Jimin Byun <jiminb159@gmail.com>
2020-06-22 06:35:45 +0000

    Add test with optional attendees and there is no gaps in their schedule

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -336,7 +336,7 @@ public final class FindMeetingQueryTest {
 
   @Test
   public void notEnoughRoomWithOptionalAttendee() {
-    // Have one person, but make it so that there is just enough room at one point in the day to
+    // Have one mandatory person, but make it so that there is just enough room at one point in the day to
     // have the meeting. The optional attendee has an event during the part of this point in the day, 
     // therefore is ignored. 
     //
@@ -387,6 +387,31 @@ public final class FindMeetingQueryTest {
         Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0800AM, false),
             TimeRange.fromStartEnd(TIME_0830AM, TIME_0900AM, false),
             TimeRange.fromStartEnd(TIME_0930AM, TimeRange.END_OF_DAY, true));
+
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void optionalAttendeeOnlyNotEnoughRoom() {
+    // Have two optional attendees with different events, but make it so that there is not enough
+    // room at any point in the day to have the meeting. No times should be returned. 
+    // 
+    // Optional: |--A--||-------B------|
+    // Day     : |---------------------|
+    // Options :
+
+    Collection<Event> events = Arrays.asList(
+        new Event("Event 1", TimeRange.fromStartDuration(TimeRange.START_OF_DAY, TIME_0900AM, false),
+            Arrays.asList(PERSON_A)),
+        new Event("Event 2", TimeRange.fromStartDuration(TIME_0900AM, TimeRange.END_OF_DAY, true),
+            Arrays.asList(PERSON_B)));
+
+    MeetingRequest request = new MeetingRequest(NO_ATTENDEES, DURATION_30_MINUTES);
+    request.addOptionalAttendee(PERSON_A);
+    request.addOptionalAttendee(PERSON_B);
+    
+    Collection<TimeRange> actual = query.query(events, request);
+    Collection<TimeRange> expected = Arrays.asList();
 
     Assert.assertEquals(expected, actual);
   }
